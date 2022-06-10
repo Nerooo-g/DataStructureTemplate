46ebd5101014da85a07a1ce1755867102152455d
Antalore G <anjun_g@msn.com>
2022-06-11 03:33:11 +0800

    Modified

--- DataStructureTemplate/MyQueue.h
+++ DataStructureTemplate/MyQueue.h
@@ -7,19 +7,21 @@ private:
 	int maxsize_;
 	int front_;
 	int rear_;
-	T* arr;
+	T* arr_;
 public:
 	MyQueue();
 	MyQueue(int maxsize);
 	void push(const T& element);
 	T pop();
+	bool is_empty();
 	int get_size();
+	~MyQueue();
 };
 
 template<class T>
 inline MyQueue<T>::MyQueue()
 {
-	arr = new T[100];
+	arr_ = new T[100];
 	front_ = rear_ = 0;
 	maxsize_ = 100;
 }
@@ -27,8 +29,8 @@ inline MyQueue<T>::MyQueue()
 template<class T>
 inline MyQueue<T>::MyQueue(int maxsize)
 {
-	maxsize_ = maxsize;
-	arr = new T[maxsize];
+	maxsize_ = ++maxsize;
+	arr_ = new T[maxsize_];
 	front_ = rear_ = 0;
 }
 
@@ -36,17 +38,23 @@ template<class T>
 inline void MyQueue<T>::push(const T& element)
 {
 	if (get_size() == maxsize_-1) throw std::overflow_error("The queue is full!");
-	arr[rear_] = element;
+	arr_[rear_] = element;
 	rear_ = (rear_ + 1) % maxsize_;
 }
 
 template<class T>
 inline T MyQueue<T>::pop()
 {
-	if (get_size() == 0) throw std::underflow_error("The queue is empty!");
-	T temp = arr[front_];
+	if (is_empty()) throw std::underflow_error("The queue is empty!");
+	T temp = arr_[front_];
 	front_ = (front_ + 1) % maxsize_;
-	return front_;
+	return temp;
+}
+
+template<class T>
+inline bool MyQueue<T>::is_empty()
+{
+	return front_ == rear_;
 }
 
 template<class T>
@@ -55,4 +63,10 @@ inline int MyQueue<T>::get_size()
 	if (rear_ == front_) return 0;
 	if (front_ < rear_) return rear_ - front_;
 	else return maxsize_ - (front_ - rear_);
+}
+
+template<class T>
+inline MyQueue<T>::~MyQueue()
+{
+	delete[] arr_;
 }
---
+++ DataStructureTemplate/MyStack.h
@@ -0,0 +1,74 @@
+#pragma once
+#include<stdexcept>
+
+template<class T>
+class MyStack {
+private:
+	T* arr_;
+	int maxsize_;
+	int top;
+public:
+	MyStack();
+	MyStack(int maxsize);
+	void push(const T& element);
+	T pop();
+	T get_top();
+	bool is_empty();
+	int get_size();
+	~MyStack();
+};
+
+template<class T>
+inline MyStack<T>::MyStack()
+{
+	arr_ = new T[100];
+	maxsize_ = 100;
+	top = -1;
+}
+
+template<class T>
+inline MyStack<T>::MyStack(int maxsize)
+{
+	maxsize_ = maxsize;
+	arr_ = new T[maxsize];
+	top = -1;
+}
+
+template<class T>
+inline void MyStack<T>::push(const T& element)
+{
+	if (top == maxsize_ - 1) throw std::overflow_error("The stack is full!");
+	arr_[++top] = element;
+}
+
+template<class T>
+inline T MyStack<T>::pop()
+{
+	if (is_empty()) throw std::underflow_error("The stack is empty!");
+	return arr_[top--];
+}
+
+template<class T>
+inline T MyStack<T>::get_top()
+{
+	if (is_empty()) throw std::underflow_error("The stack is empty!");
+	return arr_[top];
+}
+
+template<class T>
+inline bool MyStack<T>::is_empty()
+{
+	return top == -1;
+}
+
+template<class T>
+inline int MyStack<T>::get_size()
+{
+	return top+1;
+}
+
+template<class T>
+inline MyStack<T>::~MyStack()
+{
+	delete[] arr_;
+}
