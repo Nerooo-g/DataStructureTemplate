c19e5bf90af7b472c6fa326f61f823f5c092c633
Antalore G <anjun_g@msn.com>
2022-06-16 02:37:39 +0800

    Test

---
+++ DataStructureTemplate/BinarySearchTree.h
@@ -0,0 +1,149 @@
+#pragma once
+#include<iostream>
+
+template<class T>
+class BST {
+	struct Node {
+		T data;
+		Node* parent;
+		Node* left;
+		Node* right;
+		Node(T data) :parent(nullptr),left(nullptr), right(nullptr),data(data) {}
+	};
+private:
+	Node* root_;
+	Node* find_successor(Node* node);
+	void transplant(Node* ori, Node* suc);
+	Node* s_delete(Node* p);
+public:
+	void create();
+	BST(const T& data);
+	Node* search(const T& data);
+	void insert(const T& data);
+	void preorder(bool flag,Node* node = nullptr);
+	void remove(const T& data);
+	~BST();
+};
+
+template<class T>
+inline BST<T>::Node* BST<T>::find_successor(Node* node)
+{
+	if (!node||!node->right) return nullptr;
+	Node* p = node->right;
+	while (p->left) {
+		p = p->left;
+	}
+	return p;
+}
+
+template<class T>
+inline void BST<T>::transplant(Node* ori,Node* suc)
+{
+
+	if (!ori->parent) root_ = suc;
+	else if (ori == ori->parent->left) ori->parent->left = suc;
+	else ori->parent->right = suc;
+	if (suc) suc->parent = ori->parent;
+}
+
+template<class T>
+inline BST<T>::Node* BST<T>::s_delete(Node *p)
+{
+	if (p->left) p->left=s_delete(p->left);
+	if (p->right) p->right=s_delete(p->right);
+	delete p;
+	return nullptr;
+
+}
+
+
+
+template<class T>
+inline void BST<T>::create()
+{
+	T data;
+	while (cin >> data) {
+		insert(data);
+	}
+}
+
+template<class T>
+inline BST<T>::BST(const T& data)
+{
+	root_ = new Node(data);
+}
+
+template<class T>
+inline BST<T>::Node* BST<T>::search(const T& data)
+{
+	Node* p = root_;
+	while (p) {
+		if (data == p->data) return p;
+		if (data > p->data) p = p->right;
+		else p = p->left;
+	}
+	return nullptr;
+}
+
+template<class T>
+inline void BST<T>::insert(const T& data)
+{
+	Node* p = root_;
+	while (true) {
+		if (data >= p->data) {
+			if (!p->right) {
+				Node* node = new Node(data);
+				p->right = node;
+				node->parent = p;
+				break;
+			}
+			else p = p->right;
+		}
+		else {
+			if (!p->left) {
+				Node* node = new Node(data);
+				p->left = node;
+				node->parent = p;
+				break;
+			}
+			else p = p->left;
+		}
+	}
+}
+
+template<class T>
+inline void BST<T>::preorder(bool flag, Node* node)
+{
+	if (flag) node = root_;
+	if (!node) return;
+	std::cout << node->data << std::endl;
+	preorder(false, node->left);
+	preorder(false, node->right);
+}
+
+template<class T>
+inline void BST<T>::remove(const T& data)
+{
+	Node* temp = search(data);
+	if (!temp) return;
+	if (!temp->left) transplant(temp, temp->right);
+	else if (!temp->right) transplant(temp, temp->left);
+	else {
+		Node* p = find_successor(temp);
+		if (p->parent != temp) {
+			transplant(p, p->right);
+			p->right = temp->right;
+			p->right->parent = p;
+		}
+		transplant(temp, p);
+		p->left = temp->left;
+		p->left->parent = p;
+	}
+	delete temp;
+}
+
+template<class T>
+inline BST<T>::~BST()
+{
+	s_delete(root_);
+}
---
+++ DataStructureTemplate/main.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<fstream>
+#include"HashMap.h"
+#include"BinarySearchTree.h"
+using namespace std;
+
+void remove_number(string& str);
+void add_dic(const string& str, HashMap<string, int>& map) {
+    if (!map.search(str)) map.put(str, 1);
+    else map.put(str, map.get(str) + 1);
+}
+
+void split_word(const string& str,HashMap<string,int>& map){
+    string word = "";
+    for (auto x : str) {
+        if (x == ' ') {
+            if (word != "") {
+                remove_number(word);
+                add_dic(word, map);
+            }
+            word = "";
+        }
+        else word = word + x;
+    }
+    remove_number(word);
+    add_dic(word, map);
+}
+
+void remove_number(string& str) {
+    auto it = str.begin();
+    while (it != str.end()) {
+        if ((*it) >= '1' && (*it) <= '9') it = str.erase(it);
+        else it++;
+    }
+}
+
+void dic(HashMap<string, int>& map,const string& path) {
+    ifstream infile;
+    infile.open(path, ios::in);
+    string s;
+    while (!infile.eof()) {
+        getline(infile, s);
+        s.erase(remove_if(s.begin(), s.end(), ispunct), s.end());
+        split_word(s, map);
+    }
+}
+
+/*int main() {
+    BST<int>* t=new BST<int>(15);
+    /*t->insert(6);
+    t->insert(18);
+    t->insert(3);
+    t->insert(7);
+    t->insert(17);
+    t->insert(20);
+    t->insert(2);
+    t->insert(4);
+    t->insert(13);
+    t->insert(9);
+    t->remove(13);
+    t->create();
+    delete t;
+    cin.get();
+}*/
+
+int main() {
+    cout << endl;
+}
